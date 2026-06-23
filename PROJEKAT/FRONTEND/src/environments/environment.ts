71183af7159e0df47ba56e1c676cb5ff08bc1ea4
Adna Bajramović <abajramovi2@etf.unsa.ba>
2026-06-24 00:20:00 +0200

    deploy

--- PROJEKAT/FRONTEND/src/environments/environment.ts
+++ PROJEKAT/FRONTEND/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://si-grupa12-production-bdb1.up.railway.app/api',
-  keycloakUrl: 'https://keycloak-production-4c61.up.railway.app'
+  keycloakUrl: 'https://si-grupa12-production.up.railway.app'
 };
